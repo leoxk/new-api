905e473882aa3039498c33c536bab54495ee44e7
Leo Xiang <leoxiang727@gmail.com>
2026-07-18 20:35:27 +0800

    fix mobile header overflow

--- web/default/src/components/search.tsx
+++ web/default/src/components/search.tsx
@@ -38,7 +38,7 @@ export function Search({ className = '', placeholder }: SearchProps) {
     <Button
       variant='outline'
       className={cn(
-        'bg-muted/25 group text-muted-foreground hover:bg-accent relative h-8 w-full flex-1 justify-start rounded-md text-sm font-normal shadow-none sm:w-40 sm:pe-12 md:flex-none lg:w-52 xl:w-64',
+        'bg-muted/25 group text-muted-foreground hover:bg-accent relative h-8 w-8 flex-none justify-center rounded-md px-0 text-sm font-normal shadow-none sm:w-40 sm:flex-1 sm:justify-start sm:px-4 sm:pe-12 md:flex-none lg:w-52 xl:w-64',
         className
       )}
       onClick={() => setOpen(true)}
@@ -49,7 +49,7 @@ export function Search({ className = '', placeholder }: SearchProps) {
         className='absolute start-1.5 top-1/2 -translate-y-1/2'
         size={16}
       />
-      <span className='ms-4'>{resolvedPlaceholder}</span>
+      <span className='ms-4 hidden sm:inline'>{resolvedPlaceholder}</span>
       <kbd className='bg-muted group-hover:bg-accent pointer-events-none absolute end-[0.3rem] top-[0.3rem] hidden h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none sm:flex'>
         <span className='text-xs'>⌘</span>
         {t('K')}
