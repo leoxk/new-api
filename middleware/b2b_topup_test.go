d82246b00b84f8ac9c36812eed33f18b9023bbeb
Leo Xiang <leoxiang727@gmail.com>
2026-07-20 15:52:54 +0800

    use existing SQLite test driver

--- middleware/b2b_topup_test.go
+++ middleware/b2b_topup_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/model"
 	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
 	"github.com/stretchr/testify/require"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
